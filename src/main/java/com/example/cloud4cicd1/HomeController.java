a5014332549b9400b0972c2228f5e7b2286c153e
DESKTOP-974HDMP\user <sotn441579@gmail.com>
2026-06-30 10:46:55 +0900

    cicd1

--- src/main/java/com/example/cloud4cicd1/HomeController.java
+++ src/main/java/com/example/cloud4cicd1/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.cicd_1;
+package com.example.cloud4cicd1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,7 +10,7 @@ public class HomeController {
     @ResponseBody
     @GetMapping("/hi")
     public  String hi(){
-        return  "hi";
+        return  "h  ^^^^i 0630";
     }
 
 
